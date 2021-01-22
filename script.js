50f88b4e26b1cb16b9453e9007cf3b4f0a18bf29
Antoine FRANCOIS <francois.antoine3@gmail.com>
2021-01-22 11:11:05 +0100

    design the code to have just one function handling the click of the 3 parts of the flag)

--- script.js
+++ script.js
@@ -1,52 +1,28 @@
 "use strict"
 
-// var flag = ['.color-left', '.color-middle', '.color-right'];
+var flag = ['.color-left', '.color-middle', '.color-right'];
 var colors = ['blue', 'white', 'red'];
-var index = [0, 0, 0];
+var indexColors = [0, 0, 0];
 var frenchFlag = [0, 1, 2];
 
 $(function(){
-    $('.color-left').click(function() {
-        index[0]++;
-        if(index[0] == colors.length){
-            index[0] = 0;
+    $('.flag').click(function() {
+        let indexFlag = $(this).attr('number');
+        indexColors[indexFlag]++;
+        if(indexColors[indexFlag] == colors.length){
+            indexColors[indexFlag] = 0;
         }
-        if (index[0] == 0){
-            $('.color-left').removeClass(colors[colors.length - 1]);
-            $('.color-left').addClass(colors[index[0]]);
-        } else if (index[0] > 0 && index[0] < colors.length) {
-            $('.color-left').removeClass(colors[index[0] - 1]);
-            $('.color-left').addClass(colors[index[0]]);
-        }
-    })
-    $('.color-middle').click(function() {
-        index[1]++;
-        if(index[1] == colors.length){
-            index[1] = 0;
-        }
-        if (index[1] == 0){
-            $('.color-middle').removeClass(colors[colors.length - 1]);
-            $('.color-middle').addClass(colors[index[1]]);
-        } else if (index[1] > 0 && index[1] < colors.length) {
-            $('.color-middle').removeClass(colors[index[1] - 1]);
-            $('.color-middle').addClass(colors[index[1]]);
+        if (indexColors[indexFlag] == 0){
+            $(flag[indexFlag]).removeClass(colors[colors.length - 1]);
+            $(flag[indexFlag]).addClass(colors[indexColors[indexFlag]]);
+        } else if (indexColors[indexFlag] > 0 && indexColors[indexFlag] < colors.length) {
+            $(flag[indexFlag]).removeClass(colors[indexColors[indexFlag] - 1]);
+            $(flag[indexFlag]).addClass(colors[indexColors[indexFlag]]);
         }
     })
-    $('.color-right').click(function() {
-        index[2]++;
-        if(index[2] == colors.length){
-            index[2] = 0;
-        }
-        if (index[2] == 0){
-            $('.color-right').removeClass(colors[colors.length - 1]);
-            $('.color-right').addClass(colors[index[2]]);
-        } else if (index[2] > 0 && index[2] < colors.length) {
-            $('.color-right').removeClass(colors[index[2] - 1]);
-            $('.color-right').addClass(colors[index[2]]);
-        }
-    })
+   
     $('button').click(function() {
-        if (frenchFlag[0] == index[0] && frenchFlag[1] == index[1] && frenchFlag[2] == index[2]) {
+        if (frenchFlag[0] == indexColors[0] && frenchFlag[1] == indexColors[1] && frenchFlag[2] == indexColors[2]) {
             alert('ok');
         } else {
             alert('ko');
